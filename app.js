8997f105ef1b2261b29dce91608e39f4e703ebfd
Ankur Oberoi <aoberoi@gmail.com>
2011-08-14 23:06:29 -0700

    demoed server

---
+++ app.js
@@ -0,0 +1,101 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+	app = express.createServer()
+  , io = require('socket.io').listen(app);
+
+// Configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'twiddle.io'
+  });
+});
+
+app.listen(3000);
+
+// Storage
+
+var users = {
+  'asdfaasfa42432asdf' : {
+    username      : 'Ankur Oberoi',
+    password      : 'hello' 
+  }
+}
+
+// Sockets stuff
+
+io.sockets.on('connection', function (socket) {
+	
+	// ---- PHONE ----
+	
+	socket.on('phone-start', function (data) {
+	  var user = users[data.UDID];
+	  if (user == undefined) {
+	    // not a known user
+	  } else {
+	    // known user, register phone socket
+	    user.phoneSocket = socket;
+	    if (user.webSocket) {
+	      user.webSocket.emit('phone-arrive');
+	    }
+	  }
+	});
+	
+	socket.on('recieve-message', function(data) {
+	  user = users[data.UDID];
+	  user.webSocket.emit('recieve-message', { sender : data.sender, message : data.message });
+	});
+	
+	// ---- WEB ----
+	
+	socket.on('web-start', function (data) {
+	  var user = users[data.UDID];
+	  if (user == undefined) {
+	    // not a known user
+	  } else {
+	    // known user, register web socket
+	    user.webSocket = socket;
+	    user.webSocket.emit('phone-arrive');
+	    if (!user.phoneSocket) {
+	      socket.emit('wait-phone');
+	    }
+	    console.log(socket);
+	  }
+	});
+	
+	socket.on('send-message', function(data) {
+	  user = users[data.UDID];
+	  if (user != undefined ) {
+	    user.phoneSocket.emit('send-message', { recipient : data.recipient , message : data.message });
+    } else {
+      socket.emit('no-phone');
+    }
+	});
+	
+});
+
+
+
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
